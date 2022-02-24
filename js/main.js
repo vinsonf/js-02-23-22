49ab99826e0b2afb0977df0575ba216fcddf938f
vinferno <vinsonfernandez@gmail.com>
2022-02-23 21:04:05 -0800

    day1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+h1 = document.querySelector('h1');
+
+h1.innerHTML = 'Hello World!!!!!!! <br> <a href="https://www.google.com">Google</a>';
+h1.style.color = 'yellow';
+h1.classList.add('active');
+
+console.log(h1);
+
+
+
---
+++ js/syntax.js
@@ -0,0 +1,38 @@
+/*
+block comment
+*/
+
+// single line comment
+
+
+// Datatypes
+// Primitive datatypes
+alphabet       = "abcdefghijklmnopqrstuvwxyz"; // string
+     Alphabet = "abcdefghijklmnopqrstuvwxyz"; // string
+
+myBoolean = "true"; // string
+myboolean =                  "true"; // string
+myNumber = "123"; // string
+
+
+numbers = 1234567890;           // number
+yes = true; // boolean (true or false)
+no = false; // boolean (decision for condition)
+
+
+// Complex/Compound datatypes
+person = {  // object
+    name: "John",
+    age: 30,
+    isMarried: false
+};
+
+favoriteFoods = [ // array
+    "pizza",
+    "pasta",
+    "salad"
+];
+
+console.log(favoriteFoods);
+
+console.log('syntax.js');

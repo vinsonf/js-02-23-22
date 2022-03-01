a666307c67265c32baf6a633101e27fef715d11a
vinferno <vinsonfernandez@gmail.com>
2022-02-28 19:40:27 -0800

    day2

---
+++ day02/js/main.js
@@ -0,0 +1,33 @@
+// Datatypes
+// Primitives
+// String = "Hello World" | 'Hello World' | `Hello World`
+// Number = 1 | 1.1 | 1e10 | Infinity | -Infinity | NaN
+// Boolean = true | false
+
+number = Number('1');
+number = 1;
+
+string = String(1);
+string = 'Hello World';
+
+boolean = Boolean(1);
+boolean = true;
+
+
+// other primitives
+// null
+// undefined
+// Symbol
+
+
+// Complex/Compound
+// Array = [1, 2, 3] // list(s)
+// Object = { name: 'John', age: 30 } // map(s) // dictionary(s)
+
+array = Array(1, 2, 3);
+array = [{name: 'Larry', favoriteFood: [{name: 'Pizza'}]}, [], true, 1, 'Hello World'];
+
+object = Object({ name: 'John', age: 30 });
+object = { name: 'John', age: 30 };
+
+console.log(array);
--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-h1 = document.querySelector('h1');
+// h1 = document.querySelector('h1');
 
-h1.innerHTML = 'Hello World!!!!!!! <br> <a href="https://www.google.com">Google</a>';
-h1.style.color = 'yellow';
-h1.classList.add('active');
+// h1.innerHTML = 'Hello World!!!!!!! <br> <a href="https://www.google.com">Google</a>';
+// h1.style.color = 'yellow';
+// h1.classList.add('active');
 
-console.log(h1);
-
+// console.log(h1);
+console.log('main in day 1 or root')
 
 

15cab13e2bed8e9656b3bba050c8996493c22b01
vinferno <vinsonfernandez@gmail.com>
2022-03-02 21:04:48 -0800

    day3

---
+++ day03/js/math.js
@@ -0,0 +1,9 @@
+// Math object in javascript
+
+Math.PI; // 3.141592653589793
+Math.E; // 2.718281828459045
+Math.SQRT2; // 1.4142135623730951
+Math.SQRT1_2; // 0.7071067811865476
+
+
+console.log(Math.abs(7 - 3))

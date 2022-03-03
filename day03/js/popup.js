a45adf58325f5b32a28e975166a7804cf6578d6f
vinferno <vinsonfernandez@gmail.com>
2022-03-02 21:05:18 -0800

    day3 branch

---
+++ day03/js/popup.js
@@ -0,0 +1,15 @@
+
+// pop up methods
+// returns undefined
+// alert('this is a message');
+
+// confirm
+// returns true or false (ok(true) or cancel(false))
+// const answer = confirm('are you sure you want to delete this?');
+// console.log(answer);
+
+// prompt
+// returns a string
+// const num1 = prompt('what is the first Number?');
+// const num2 = prompt('what is the second Number?');
+console.log(+num1 + +num2);
---
+++ day03/js/strings.js
@@ -0,0 +1,36 @@
+// functions
+function add(a, b, c, d) { // a, b, c, d are optional parameters
+    return a + b + c + d; // return the sum of the parameters
+}
+
+
+// function vs method
+// function is a block of code that can be called from anywhere
+// method is a block of code that can only be called from inside a object
+
+// list of string methods
+// at
+// charAt
+// charCodeAt
+// concat
+// endsWith
+// fromCharCode
+// includes
+// indexOf
+// lastIndexOf
+// localeCompare
+// match
+// repeat
+// replace
+// search
+// slice
+// split
+// startsWith
+// substr
+// substring
+// toLocaleLowerCase
+// toLocaleUpperCase
+// toLowerCase
+// toUpperCase
+// trim
+// valueOf
